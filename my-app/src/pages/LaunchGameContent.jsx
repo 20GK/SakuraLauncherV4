b3377bab8b324d5d30fefa3d68c923e576aad727
20GK <ddezmonsd@gmail.com>
2023-03-01 20:48:01 +0200

    upd BETA v2.1.0

--- my-app/src/pages/LaunchGameContent.jsx
+++ my-app/src/pages/LaunchGameContent.jsx
@@ -6,6 +6,7 @@ export default function LaunchGameContent() {
   const locate = useLocation()
 
   const [logs, setLogs] = useState([])
+  const [progress, setProgress] = useState()
 
   useEffect(() => {    
     async function launch() {
@@ -27,24 +28,42 @@ export default function LaunchGameContent() {
     console.log(data)
     if(data[0].includes('[MCLC]: Using Java version')){
       setLogs(['Так-с, я нашел Java'])
+      setProgress('5%')
+    } else if(data[0].includes('[MCLC]: Attempting to create root folder')){
+      setLogs(['Создаем папку'])
+      setProgress('15%')
+    } else if(data[0].includes('[MCLC]: Parsed version from version manifest')){
+      setLogs(['Ищем нужную версию майнкрафта'])
+      setProgress('28%')
+    } else if(data[0].includes('[MCLC]: Attempting to download Minecraft version jar')){
+      setLogs(['Загружаем майнкрафт JAR ядро'])
+      setProgress('42%')
+    } else if(data[0].includes('[MCLC]: Downloaded version jar and wrote version json')){
+      setLogs(['Нужное ядро JAR загружено'])
+      setProgress('45%')
     } else if(data[0].includes('[MCLC]: Collected class paths')){
       setLogs(['Ищем классы и пути...'])
+      setProgress('50%')
     } else if(data[0].includes('[MCLC]: Using ; to separate class paths')){
       setLogs(['Исправляем и загружаем нужные библиотеки'])
+      setProgress('55%')
     } else if(data[0].includes('[MCLC]: Attempting to download assets')){
-      setLogs(['Попытка загрузки ассетов'])
+      setLogs(['Загружаем нужные ассеты'])
+      setProgress('60%')
     } else if(data[0].includes('[MCLC]: Downloaded assets')){
-      setLogs(['Ура, загружаем ассеты'])
+      setLogs(['Ура, ассеты загрузились'])
+      setProgress('65%')
     } else if(data[0].includes('[MCLC]: Set launch options')){
       setLogs(['Выставляем нужные шестеренки для запуска'])
+      setProgress('90%')
     } else if(data[0].includes('[MCLC]: Launching with arguments')){
       setLogs(['Заводим двигатель, игра запускается!'])
+      setProgress('100%')
       setTimeout(() => {
         goCloseApp()
       }, 3000);
     }
   })
-  
 
   return (
     <>
@@ -56,7 +75,7 @@ export default function LaunchGameContent() {
             <textarea readOnly={true} autoFocus={false} value={logs.join('\n')}></textarea>
           </div>
           <div className='Progress-Bar'>
-            <div className='Progress-Bar-Value' style={{width: `50%`}}>50%</div>
+            <div className='Progress-Bar-Value' style={{width: progress}}>{progress}</div>
           </div>
         </div>
       </div>
